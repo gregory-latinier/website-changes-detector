b5b98edf8245ec861e8fa8c7d47d0a81b085f895
gregory-latinier <gregory.latinier@gmail.com>
2020-09-04 11:42:57 +0200

    Log monitoring

--- packages/api/src/services/monitoring/monitoring.class.ts
+++ packages/api/src/services/monitoring/monitoring.class.ts
@@ -42,62 +42,66 @@ export class Monitoring implements ServiceMethods<Data> {
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async find (params?: Params): Promise<Data[] | Paginated<Data>> {
-    if(!this.browser) {
-      this.browser = await puppeteer.launch();
-    }
-    const urls = await this.app.service('urls').find({ query: { active: true } }) as Paginated<IUrl>;
-    if(urls.data.length) {
-      for(const url of urls.data) {
-        // The Url can be check
-        if(!url.lastCheck || url.lastCheck.getTime() <= Date.now() - url.frequency * url.frequencyUnit) {
-          await this.app.service('urls').patch(url._id, { lastCheck: Date.now() });
-          const page = await this.browser.newPage();
-          await page.goto(url.url);
-          await page.screenshot({path: `${this.app.get('screenshotPath')}/${url._id}-new.png`});
-          if(!fs.existsSync(`${this.app.get('screenshotPath')}/${url._id}.png`)) {
-            fs.renameSync(`${this.app.get('screenshotPath')}/${url._id}-new.png`, `${this.app.get('screenshotPath')}/${url._id}.png`);
-          } else {
-            const newImg = PNG.sync.read(fs.readFileSync(`${this.app.get('screenshotPath')}/${url._id}-new.png`));
-            const oldImg = PNG.sync.read(fs.readFileSync(`${this.app.get('screenshotPath')}/${url._id}.png`));
-            const diff = pixelmatch(newImg.data, oldImg.data, null, 800, 600, { threshold: 0.1 });
-            if(diff > 0) {
-              await this.app.service('urls').patch(url._id, { lastAlert: Date.now() });
-              fs.unlinkSync(`${this.app.get('screenshotPath')}/${url._id}.png`);
+    try {
+      if(!this.browser) {
+        this.browser = await puppeteer.launch();
+      }
+      const urls = await this.app.service('urls').find({ query: { active: true } }) as Paginated<IUrl>;
+      if(urls.data.length) {
+        for(const url of urls.data) {
+          // The Url can be check
+          if(!url.lastCheck || url.lastCheck.getTime() <= Date.now() - url.frequency * url.frequencyUnit) {
+            await this.app.service('urls').patch(url._id, { lastCheck: Date.now() });
+            const page = await this.browser.newPage();
+            await page.goto(url.url);
+            await page.screenshot({path: `${this.app.get('screenshotPath')}/${url._id}-new.png`});
+            if(!fs.existsSync(`${this.app.get('screenshotPath')}/${url._id}.png`)) {
               fs.renameSync(`${this.app.get('screenshotPath')}/${url._id}-new.png`, `${this.app.get('screenshotPath')}/${url._id}.png`);
+            } else {
+              const newImg = PNG.sync.read(fs.readFileSync(`${this.app.get('screenshotPath')}/${url._id}-new.png`));
+              const oldImg = PNG.sync.read(fs.readFileSync(`${this.app.get('screenshotPath')}/${url._id}.png`));
+              const diff = pixelmatch(newImg.data, oldImg.data, null, 800, 600, { threshold: 0.1 });
+              if(diff > 0) {
+                await this.app.service('urls').patch(url._id, { lastAlert: Date.now() });
+                fs.unlinkSync(`${this.app.get('screenshotPath')}/${url._id}.png`);
+                fs.renameSync(`${this.app.get('screenshotPath')}/${url._id}-new.png`, `${this.app.get('screenshotPath')}/${url._id}.png`);
 
-              const alerts = await this.app.service('alerts').find({ query: { active: true } }) as Paginated<IAlert>;
-              if(alerts.data.length) {
-                for (const alert of alerts.data) {
-                  if(alert.type === 'sms') {
-                    await this.twilio.messages.create({
-                      from: this.app.get('twilio').number,
-                      to: alert.value,
-                      body: `Changement détecté sur ${url.title}: ${url.url}`
-                    });
-                  } else if(alert.type === 'email') {
-                    const msg = {
-                      to: alert.value,
-                      from: this.app.get('sendGrid').sender,
-                      subject: url.title,
-                      text: `Changement détecté sur ${url.title}: ${url.url}`,
-                      html: `Changement détecté sur <a href="${url.url}">${url.title}</a>`,
-                    };
-                    await sgMail.send(msg);
-                  } else if(alert.type === 'whatsapp') {
-                    await this.twilio.messages.create({
-                      from: this.app.get('twilio').whatsapp,
-                      to: alert.value,
-                      body: `Changement détecté sur ${url.title}: ${url.url}`
-                    });
+                const alerts = await this.app.service('alerts').find({ query: { active: true } }) as Paginated<IAlert>;
+                if(alerts.data.length) {
+                  for (const alert of alerts.data) {
+                    if(alert.type === 'sms') {
+                      await this.twilio.messages.create({
+                        from: this.app.get('twilio').number,
+                        to: alert.value,
+                        body: `Changement détecté sur ${url.title}: ${url.url}`
+                      });
+                    } else if(alert.type === 'email') {
+                      const msg = {
+                        to: alert.value,
+                        from: this.app.get('sendGrid').sender,
+                        subject: url.title,
+                        text: `Changement détecté sur ${url.title}: ${url.url}`,
+                        html: `Changement détecté sur <a href="${url.url}">${url.title}</a>`,
+                      };
+                      await sgMail.send(msg);
+                    } else if(alert.type === 'whatsapp') {
+                      await this.twilio.messages.create({
+                        from: this.app.get('twilio').whatsapp,
+                        to: alert.value,
+                        body: `Changement détecté sur ${url.title}: ${url.url}`
+                      });
+                    }
                   }
                 }
               }
             }
-          }
 
-          await page.close();
+            await page.close();
+          }
         }
       }
+    } catch (e) {
+      console.log(e);
     }
     return [];
   }
